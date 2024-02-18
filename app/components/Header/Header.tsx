b4a061c70ef05b76dc122cbb8c1277f87aaa17aa
nunof <fernandes.n.sa@outlook.fr>
2024-02-18 08:25:54 +0100

    start hero section

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,29 +1,42 @@
 "use client";
 import Image from "next/image";
-import React from "react";
+import React, { useRef } from "react";
 
 import logo from "@/public/logo.png";
 import navLinks from "@/app/constants/navLinks";
 import Button from "../Button/Button";
-import { FaRocket, FaWallet } from "react-icons/fa";
+import HeaderImage from "@/public/headerImages/header-image.jpg";
+import hover3D from "@/app/utils/hover";
+
+import { IoMdImages } from "react-icons/io";
+import { BiMailSend } from "react-icons/bi";
+import { RiAccountPinCircleFill } from "react-icons/ri";
+import { Abril_Fatface } from "next/font/google";
+
 import styled from "styled-components";
+import Hover3d from "@/app/utils/hover";
+
+//* FONT "Abril_Fatface"
+const abril = Abril_Fatface({ subsets: ["latin"], weight: ["400"] });
 
 const Header = () => {
+    const hero = useRef<HTMLDivElement>(null);
+
+    const hoverHero = Hover3d(hero, { x: 20, y: -5, z: 15 });
+
     return (
-        <HeaderStyled>
+        <HeaderStyled ref={hero}>
+            {/* -----NAVBAR----- */}
             <nav>
                 <div className="logo">
-                    <Image src={logo} alt="logo" width={36} height={36} />
-                    <h2>Logo</h2>
-                </div>
-
-                <div className="input">
-                    <input
-                        type="search"
-                        name="search"
-                        id="search"
-                        placeholder="Search"
+                    <Image
+                        src={logo}
+                        alt="logo"
+                        width={36}
+                        height={36}
+                        title="logo"
                     />
+                    <h2>Logo</h2>
                 </div>
 
                 <ul className="nav-items">
@@ -33,14 +46,78 @@ const Header = () => {
                         </li>
                     ))}
 
-                    <Button icon={<FaWallet />} name="Connect Wallet" />
+                    <Button icon={<RiAccountPinCircleFill />} name="Login" />
                 </ul>
             </nav>
+            {/* -----END NAVBAR----- */}
+
+            {/* -----HEADER CONTENT----- */}
+            <div className="header-content">
+                <div className="text-content">
+                    <h1 className={abril.className}>
+                        Conception Innovante, Créativité Architecturale
+                    </h1>
+
+                    <div className="text-paragraph">
+                        <p>
+                            Nous sommes passionnés par la conception
+                            d&apos;espaces uniques qui transcendent
+                            l&apos;esthétique pour créer des expériences
+                            authentiques. Avec une expertise reconnue dans la
+                            conception de bâtiments résidentiels, commerciaux et
+                            institutionnels, notre équipe s&apos;engage à
+                            transformer vos rêves en réalité.
+                        </p>
+                        <p>
+                            Explorez notre portefeuille diversifié et découvrez
+                            comment nous donnons vie à des concepts audacieux
+                            tout en respectant les lignes directrices du design
+                            durable.
+                        </p>
+                        <p>
+                            Bienvenue dans notre univers, où chaque projet est
+                            une histoire inédite à raconter.
+                        </p>
+                    </div>
+
+                    <div className="header-buttons">
+                        <Button
+                            name="Voir Portfolio"
+                            background="#5f3918"
+                            color="#ddd"
+                            border="1px solid #f2994a"
+                            icon={<IoMdImages />}
+                        />
+
+                        <Button name="Nous contacter" icon={<BiMailSend />} />
+                    </div>
+                </div>
+
+                <div className="image-content">
+                    {/* image1 */}
+                    <div
+                        className="image"
+                        style={{
+                            transform: hoverHero.transform,
+                            transition: hoverHero.transition,
+                        }}
+                    >
+                        <Image
+                            src={HeaderImage}
+                            alt={"Image d'une de nous constructions"}
+                            title=""
+                            width={500}
+                            height={500}
+                        />
+                    </div>
+                </div>
+            </div>
+            {/* -----END OF HEADER CONTENT----- */}
         </HeaderStyled>
     );
 };
 
-//* styled component
+//* ----- styled component -----
 const HeaderStyled = styled.header`
     nav {
         padding: 0 4rem;
@@ -57,24 +134,6 @@ const HeaderStyled = styled.header`
             cursor: pointer;
         }
 
-        .input {
-            flex: 2;
-            display: flex;
-            justify-content: center;
-            input {
-                width: 55%;
-                padding: 0.6rem 0.8rem;
-                border-radius: 8px;
-                background-color: var(--color-input);
-                border: 1px solid var(--color-border);
-
-                &:placeholder {
-                    color: var(--color-border);
-                    font-weight: 500;
-                }
-            }
-        }
-
         .nav-items {
             display: flex;
             align-items: center;
@@ -84,12 +143,56 @@ const HeaderStyled = styled.header`
 
                 &:hover {
                     color: white;
-                    transform: scale(1.1);
+                    transform: scale(1.05);
                 }
                 a {
                     padding: 0.3rem;
+                    &:hover {
+                        color: var(--secondary-color);
+                    }
                 }
             }
+        }
+    }
+
+    .header-content {
+        display: grid;
+        grid-template-columns: 50% 50%;
+        align-items: center;
+        gap: 4rem;
+        min-height: calc(100vh - 10vh);
+        padding: 0 5rem;
+    }
+
+    .text-content {
+        > h1 {
+            font-size: clamp(1rem, 5vw, 4rem);
+            color: var(--secondary-color);
+            transition: all 0.01s linear;
+            padding-bottom: 1.5rem;
+        }
+
+        .header-buttons {
+            display: flex;
+            gap: 1.5rem;
+            margin-top: 2.5rem;
+        }
+    }
+
+    .image-content .image {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        img {
+            border-radius: 8px;
+        }
+    }
+
+    /* MEDIA QUERY */
+    @media screen and (max-width: 1024px) {
+        .header-content {
+            display: grid;
+            grid-template-columns: 100%;
         }
     }
 `;
--- app/constants/navLinks.js
+++ app/constants/navLinks.js
@@ -1,8 +1,7 @@
 const navLinks = [
     { id: 1, name: "Home" },
-    { id: 2, name: "Auctions" },
-    { id: 3, name: "Marketplace" },
-    { id: 4, name: "About" },
+    { id: 2, name: "About" },
+    { id: 3, name: "Portfolio" },
 ];
 
 export default navLinks;
